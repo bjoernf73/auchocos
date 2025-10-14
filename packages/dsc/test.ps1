4be24314175b3e2c699889602013b8cfe7edb19a
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2025-10-14 18:14:39 +0200

    added test script

---
+++ packages/dsc/test.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$Package,
+    [string]$Version,
+    [string]$NuspecPath
+)
+try{
+    $DscVersionReturn = & dsc.exe --version
+    if($LASTEXITCODE -ne 0){
+        throw "dsc.exe --version failed with exit code $LASTEXITCODE"
+    }
+
+    if($DscVersionReturn -eq "dsc $version"){
+        Write-Host "dsc: test passed. Version '$Version' is installed."
+        return $true
+    }
+    else{
+        Write-Host "dsc: test failed. dsc.exe --version returned '$DscVersionReturn' instead of 'dsc $version'."
+        return $false
+    }
+}
+catch{
+    throw "$_"
+}
--- update_all.ps1
+++ update_all.ps1
@@ -7,19 +7,43 @@ try{
     )
 
     foreach($Package in $Packages){
-        $ThisPackagePath = Join-Path $PackagesPath -ChildPath $Package
+        $ThisPackagePath = Join-Path -Path $PackagesPath -ChildPath $Package
+        $ThisPackageNuspec = Join-Path -Path $ThisPackagePath -ChildPath "$Package.nuspec"
+        $ThisPackageUpdate = Join-Path -Path $ThisPackagePath -ChildPath 'update.ps1'
+        $ThisPackageTest = Join-Path -Path $ThisPackagePath -ChildPath 'test.ps1'
         Set-Location -Path $ThisPackagePath
         try{
-            $uResult = & .\update.ps1
+            $uResult = $null
+            $tResult = $null
+
+            $uResult = & $ThisPackageUpdate
             if($true -eq $uResult.WasUpdated){
-                $tResult = & .\test.ps1 -uResult $uResult
+                Write-Host "$($Package): Update available."
+                try{
+                    Write-Host "$($Package): Testing nuspec '$ThisPackageNuspec'"
+                    Test-Package -Nu "$ThisPackageNuspec" -Install
+                }
+                catch{
+                    $PSCmdlet.ThrowTerminatingError($_)
+                }
+                # run package specific tests
+                $tResultParams = @{
+                    Package = $Package
+                    Version = $uResult.Version
+                    NuspecPath = $ThisPackageNuspec
+                }
+                $tResult = & $ThisPackageTest @tResultParams
+            }
+            elseif($false -eq $uResult.WasUpdated){
+                Write-Host "$($Package): No update."
             }
             else{
-                # test not needed, no update
+                Write-Host "$($Package): Failed to run."
+                throw "$($Package): Failed to run scripts."
             }
         }
         catch{
-            Write-Error "Error updating package '$Package': $_"
+            Write-Error "$($Package): Error updating package: $_"
         }
     }
 }
