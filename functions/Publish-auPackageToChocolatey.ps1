eb79151d9058931a76a7efb5059535a4852339a0
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2025-11-20 00:55:49 +0100

    new try

--- functions/Publish-auPackageToChocolatey.ps1
+++ functions/Publish-auPackageToChocolatey.ps1
@@ -7,8 +7,9 @@ function Publish-auPackageToChocolatey {
 
     try {
         $Nupkg = Get-Item -Path "$pwd\*" -Include "*.nupkg"
-        choco setapikey $env:CHOCO_API_KEY -s https://push.chocolatey.org/
-        choco push $($Nupkg.name) -s https://push.chocolatey.org/
+        #choco setapikey $env:CHOCO_API_KEY -s https://push.chocolatey.org/
+        #choco push $($Nupkg.name) -s https://push.chocolatey.org/
+        & choco push $($NuPkg.Name) --source https://push.chocolatey.org/ --api-key $env:CHOCO_API_KEY
     
         if ($LASTEXITCODE -eq 0) {
             Write-Host "$($Package): Published choco '$($NuPkg.Name)'"
