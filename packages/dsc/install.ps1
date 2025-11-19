679265cad27cf3a2b852ff806082efee9c86df23
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2025-11-19 22:39:14 +0100

    test install

--- packages/dsc/install.ps1
+++ packages/dsc/install.ps1
@@ -3,16 +3,34 @@ param(
     [string]$Version,
     [string]$NuspecPath
 )
+
+$iResult = [PSCustomObject]@{
+    Package = $Package
+    Version = $Version
+    InstallSuccess = $false
+    PackSuccess = $false
+    Exception = $null
+}
 try{
     Write-Host "$($Package): Testing nuspec '$NuspecPath'"
-    Write-Host "$($Package): Current directory '$pwd'"
-    if(Test-Path -Path $NuspecPath){
-        Write-Host "$($Package): Found nuspec file."
+    # first, create the package
+    & choco pack $NuspecPath --no-progress --confirm
+    if($LASTEXITCODE -ne 0){
+        $iResult.Exception = "$($Package): choco pack failed with exit code $LASTEXITCODE"
+        return $iResult
     }
-    else{
-        throw "$($Package): Nuspec file '$ThisPackageNuspec' not found."
+    $iResult.PackSuccess = $true
+    Write-Host "$($Package): choco pack success."
+    
+    & choco install $Package --version "$Version" --source "$pwd" --force --no-progress --confirm
+    if($LASTEXITCODE -ne 0){
+        $iResult.Exception = "$($Package): choco install failed with exit code $LASTEXITCODE"
+        return $iResult
     }
+    $iResult.InstallSuccess = $true
+    Write-Host "$($Package): choco install success."
+    return $iResult
 }
 catch{
-    $PSCmdlet.ThrowTerminatingError($_)
+    throw $_
 }
