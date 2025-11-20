f0b6dbde3b569268db9b40c38db1789a1d01df71
Bjørn Henrik Formo aka databjørn <bjornhenrikformo@gmail.com>
2025-11-20 22:32:31 +0100

    bugfix3

--- functions/Publish-auPackageToGit.ps1
+++ functions/Publish-auPackageToGit.ps1
@@ -11,13 +11,22 @@ function Publish-auPackageToGit {
         [string]$Message
     )
     try {
-        
-        
-
         Set-Location $PSScriptRoot
-        $null = & git add . 
+        Write-Host "$($Package): Current path '$pwd'"
+        Write-Host "$($Package): > Adding changes to git."
+        $null = & git add .
+        
+        Write-Host "$($Package): > Committing changes to git."
         $gitCommitOut = & git commit -m "AppVeyor build update: Package $($Package) updated to version $($uResult.Version.ToString())"
+        for($c = 0; $c -lt $gitCommitOut.Length; $c++){
+            Write-Host "$($Package): git commit out: $($gitCommitOut[$c])"
+        }
+
+        Write-Host "$($Package): > Pushing changes to git."
         $gitPushOut = & git push origin HEAD:main
+        for($c = 0; $c -lt $gitPushOut.Length; $c++){
+            Write-Host "$($Package): git commit out: $($gitPushOut[$c])"
+        }
 
         # todo: check output against known success messages
         if($LASTEXITCODE -eq 0){
